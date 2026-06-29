887832e0b3f2ca5f4407440fb81ed4bb2a5bee97
yaelmarciano <yael.marciano0@dauphine.eu>
2026-06-29 12:20:21 +0200

    Update 1_Axes.Ruissellement.95.py

--- pages/1_Axes.Ruissellement.95.py
+++ pages/1_Axes.Ruissellement.95.py
@@ -24,7 +24,7 @@ print("Chargement des contours EPCI...")
 try:
     gdf_epci = gpd.read_file("epci-100m.geojson", engine="fiona")
 except Exception:
-    gdf_epci = gpd.read_file("epci-100m.geojson",driver="GeoJSON")
+    gdf_epci = gpd.read_file("epci-100m.geojson")
 
 # Conversion en degrés pour Folium
 gdf_epci_4326 = gdf_epci.to_crs(epsg=4326)
